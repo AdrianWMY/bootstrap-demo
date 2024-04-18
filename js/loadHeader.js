e299169f95295c1c6d1028683cf1924ffad7f668
adrian <adrianwang911@gmail.com>
2024-04-18 12:19:40 +1000

    update

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -1,7 +1,7 @@
 // loadHeader.js
 $(function () {
     // Load the header HTML file using jQuery's load() method
-    $('#header-placeholder').load('/public/header.html', function (response, status, xhr) {
+    $('#header-placeholder').load('public/header.html', function (response, status, xhr) {
         if (status === 'error') {
             console.error('Error loading header:', xhr.statusText);
         }
